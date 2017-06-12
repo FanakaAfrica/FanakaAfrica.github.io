46ad05b124bd18c4bfbec983dfd9ea37730c0d87
FanakaAfrica <dkinyua@wharton.upenn.edu>
2017-06-13 00:08:25 +0300

    Add files via upload

---
+++ foo.js
@@ -0,0 +1,44 @@
+
+
+$(document).ready(function(){
+
+	/*myFunction('.container');*/
+	$(".side_menu").css("font-size","10px");
+	$(".side_menu").css("width","28%");
+	$(".titles").css("font-size","25px");
+	$("#text").css("font-size","14px");
+
+	$(".logo").css("width", "90px");
+	$(".logo").css("height", "50px");
+	$(".logo").css("margin-top", "30px");
+
+	
+	$(".logo").click(function(){
+		window.location = 'index.html' ; 
+	});
+
+
+
+	/*$(".side_menu").mouseenter(function(){
+		$(".mycontent").css('background-color', 'rgba(0,0,0,0.1)');
+		$("mark").css('background-color', 'rgba(0,0,0,0.009)');
+		$("#shaded").css('background-color', 'rgba(0,0,0,0.009)');
+		$("#ulshaded").css('background-color', 'rgba(0,0,0,0.009)');
+
+	});
+	$(".side_menu").mouseleave(function(){
+		$(".mycontent").css('background-color', 'white');
+		$("mark").css('background-color', 'white');
+		$("#shaded").css('background-color', '#F7F6F7');
+		$("#ulshaded").css('background-color', '#F7F6F7');
+	
+	});*/
+	
+});
+
+
+	
+
+	
+
+	
