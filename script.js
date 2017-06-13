6f9a4dc36d4653263b4a8883c6f28c9f5f6d5780
FanakaAfrica <dkinyua@wharton.upenn.edu>
2017-06-13 13:08:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,9 @@
 
 $(document).ready(function(){
 
-	/*myFunction('.container');*/
-
-	
 	$(".logo").click(function(){
 		window.location = 'index.html' ; 
 	});
-	/*$(".side_menu").mouseenter(function(){
-		$(".mycontent").css('background-color', 'rgba(0,0,0,0.1)');
-		$("mark").css('background-color', 'rgba(0,0,0,0.009)');
-		$("#shaded").css('background-color', 'rgba(0,0,0,0.009)');
-		$("#ulshaded").css('background-color', 'rgba(0,0,0,0.009)');
-
-	});
-	$(".side_menu").mouseleave(function(){
-		$(".mycontent").css('background-color', 'white');
-		$("mark").css('background-color', 'white');
-		$("#shaded").css('background-color', '#F7F6F7');
-		$("#ulshaded").css('background-color', '#F7F6F7');
-	
-	});*/
 	
 });
 
